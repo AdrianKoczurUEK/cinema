ba31237fad2e8f55d5f544ed25504149a9fbb871
AdrianKoczurUEK <s219487@student.uek.krakow.pl>
2025-01-11 14:00:05 +0100

    ddd

---
+++ precache-manifest.9ae8c1a85bf84ee8634faa119a4de25b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c1ae4e702e863f6b490e1cac38625945",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5d3b5e2d2e96705caf98",
+    "url": "/static/css/main.94a30fe4.chunk.css"
+  },
+  {
+    "revision": "3ad862602cb5a37cdea5",
+    "url": "/static/js/2.19359a8c.chunk.js"
+  },
+  {
+    "revision": "5d3b5e2d2e96705caf98",
+    "url": "/static/js/main.2f6ac5f3.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
--- src/components/AppHeader.vue
+++
@@ -1,7 +0,0 @@
-<template>
-    <div class="navbar navbar-inverse bg-dark"> 
-        <div class="container d-flex justify-content-between">
-            <router-link to="/" class="navbar-brand">Vizyon</router-link>
-        </div>
-    </div>
-</template>
