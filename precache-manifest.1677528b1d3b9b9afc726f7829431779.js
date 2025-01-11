78752924d757a9921b10f69313fec701a2eca816
AdrianKoczurUEK <s219487@student.uek.krakow.pl>
2025-01-11 14:18:03 +0100

    xxx

---
+++ precache-manifest.1677528b1d3b9b9afc726f7829431779.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c8c8e685acc8ddc326ba44c1bd6bef22",
+    "url": "/cinema/index.html"
+  },
+  {
+    "revision": "5d3b5e2d2e96705caf98",
+    "url": "/cinema/static/css/main.94a30fe4.chunk.css"
+  },
+  {
+    "revision": "3ad862602cb5a37cdea5",
+    "url": "/cinema/static/js/2.19359a8c.chunk.js"
+  },
+  {
+    "revision": "5d3b5e2d2e96705caf98",
+    "url": "/cinema/static/js/main.2f6ac5f3.chunk.js"
+  },
+  {
+    "revision": "b867071ff6b861045f0a",
+    "url": "/cinema/static/js/runtime~main.069b81f7.js"
+  }
+]);
